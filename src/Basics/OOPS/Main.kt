142408425cae7be2ee0553a11029f01a0b6c971e
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-02-04 10:56:41 +0530

    just codes

---
+++ src/Basics/OOPS/Main.kt
@@ -0,0 +1,46 @@
+package Basics.OOPS
+
+import java.util.*
+import kotlin.collections.ArrayList
+
+fun main(){
+
+    val name:String = readLine().toString()
+    val basicSalary:Double = readLine()!!.toDouble()
+    val k = Manager(name,basicSalary)
+    k.doubleGrossSal()
+    println(arr[1])
+
+}
+
+
+data class db(val name: String,val ba:Double,val da:Double, val hra:Double, val it:Double)
+
+internal var arr = ArrayList<db>()
+
+abstract class Employee constructor(name:String, ba:Double ) {
+    private val Name: String = name
+    private val basic_sal: Double = ba
+    protected val da: Double = basic_sal * 0.7
+    protected val hra: Double = basic_sal * 0.3
+    protected val it: Double = basic_sal * 0.4
+    open fun doubleGrossSal(){}
+
+   open fun addToDb(){
+        var k = db(Name,basic_sal,da,hra,it)
+        arr.add(k)
+    }
+}
+
+class Manager(name:String,ba:Double):Employee(name,ba){
+
+    init {
+        super.addToDb()
+    }
+    override fun doubleGrossSal() {
+        val doubleGrossSald:Double = super.da+ super.hra
+
+        println(doubleGrossSald)
+    }
+
+}
